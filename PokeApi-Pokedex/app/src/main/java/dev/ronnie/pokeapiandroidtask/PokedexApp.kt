9d38189ba7c7b04bff0b247bbff090e614079d62
Leo Ando <ando.reo420@gmail.com>
2021-08-08 19:09:02 +0900

    mend

--- PokeApi-Pokedex/app/src/main/java/dev/ronnie/pokeapiandroidtask/PokedexApp.kt
+++ PokeApi-Pokedex/app/src/main/java/dev/ronnie/pokeapiandroidtask/PokedexApp.kt
@@ -3,9 +3,5 @@ package dev.ronnie.pokeapiandroidtask
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-
-/**
- *created by Ronnie Otieno on 20-Dec-20.
- **/
 @HiltAndroidApp
-class PokemonApp : Application()
+class PokedexApp : Application()
